9b51f36b91ec1da272b591c7948cf7a0c4be0bcc
Eric Easthope <eric.easthope@me.com>
2021-11-08 01:18:59 -0800

    Add BibTeX parser, move Application component

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,33 @@
 //
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import Application from "./components/Application";
+import "index.scss";
+
+import { parseBibFile, normalizeFieldValue } from "bibtex";
+
+const bibFile = parseBibFile(`
+    @InProceedings{realscience,
+      author    = {Marteen Fredrik Adriaan ding de la Trumppert and مهدي N\\"allen and henQuq, jr, Mathize},
+      title     = {You Won't Believe This Proof That {P} \\gtreqqless {NP} Using Super-{T}uring Computation Near Big Black Holes},
+      booktitle = {Book of Qeq},
+      month     = {September},
+      year      = {2001},
+      address   = {Dordrecht},
+      publisher = {Willems Uitgeverij},
+      url       = {https://github.com/digitalheir/},
+      pages     = {6--9}
+    }
+`);
+
+const Application = () => {
+  React.useEffect(() => {
+    console.log(bibFile.entries$);
+    console.log(bibFile.entries$.realscience.type);
+    console.log(bibFile.entries$.realscience.fields);
+    console.log(bibFile.entries$.realscience.fields.author.authors$);
+  }, []);
+
+  return <div className="Application">Hello, World!</div>;
+};
 
 ReactDOM.render(<Application />, document.getElementById("root"));
