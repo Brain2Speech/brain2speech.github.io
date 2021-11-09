d07c437f9b34d8061048d52002af82e82fc22e20
Eric Easthope <eric.easthope@me.com>
2021-11-08 21:33:18 -0800

    Add fonts, add sections, add scroller prototype

--- src/index.tsx
+++ src/index.tsx
@@ -4,20 +4,10 @@ import * as ReactDOM from "react-dom";
 import "index.scss";
 
 import { parseBibFile, normalizeFieldValue } from "bibtex";
+import bib from "../data/refs.txt";
+import bios from "../data/bios.json";
 
-const bibFile = parseBibFile(`
-    @InProceedings{realscience,
-      author    = {Marteen Fredrik Adriaan ding de la Trumppert and مهدي N\\"allen and henQuq, jr, Mathize},
-      title     = {You Won't Believe This Proof That {P} \\gtreqqless {NP} Using Super-{T}uring Computation Near Big Black Holes},
-      booktitle = {Book of Qeq},
-      month     = {September},
-      year      = {2001},
-      address   = {Dordrecht},
-      publisher = {Willems Uitgeverij},
-      url       = {https://github.com/digitalheir/},
-      pages     = {6--9}
-    }
-`);
+const bibFile = parseBibFile(bib);
 
 const Application = () => {
   React.useEffect(() => {
@@ -25,9 +15,96 @@ const Application = () => {
     console.log(bibFile.entries$.realscience.type);
     console.log(bibFile.entries$.realscience.fields);
     console.log(bibFile.entries$.realscience.fields.author.authors$);
+
+    const body = document.getElementsByTagName("body")[0];
+    body.onscroll = e => {
+      //stackoverflow.com/a/14384091
+      let top = window.pageYOffset || document.documentElement.scrollTop;
+      console.log(top);
+    };
   }, []);
 
-  return <div className="Application">Hello, World!</div>;
+  return (
+    <div className="Application">
+      <header className="Header">
+        <ScrollButton name="Project" />
+        <ScrollButton name="Team" />
+        <ScrollButton name="Papers" />
+        <ScrollButton name="Contact" />
+      </header>
+      <main>
+        <h1>Brain2Speech</h1>
+        <img alt="Brain-to-speech landing image" />
+        <h2>Project</h2>
+        <p>
+          The Brain2Speech project seeks to build and validate new methods for
+          advanced speech synthesis and control using neural activation-based
+          models and 3D biomechanical articulators.
+        </p>
+        <h2>Team</h2>
+        <div className="Bios">
+          {bios.map((bio, index) => (
+            <Bio key={`bio-${index}`} {...bio} />
+          ))}
+        </div>
+
+        <h2>Papers</h2>
+        <p>...</p>
+        <h2>Contact</h2>
+        <p className="center">For press inquires reach us directly at ...</p>
+        <footer>
+          <div>Copyright © Brain2Speech 2021</div>
+          <div>
+            <a className="" href="">
+              License
+            </a>
+          </div>
+        </footer>
+      </main>
+    </div>
+  );
+};
+
+const Bio = ({ img, name, title, content }) => {
+  return (
+    <div className="Bio">
+      <img alt={`Photo of ${name}`} src={img} />
+      <div className="name">{name}</div>
+      <div className="title">{title}</div>
+      <div className="content">{content}</div>
+    </div>
+  );
+};
+
+const ScrollButton = ({ name }) => {
+  return (
+    <span
+      className="ScrollButton"
+      onClick={e => {
+        const headers = document.getElementsByTagName("h2");
+        for (let i = 0; i < headers.length; i++) {
+          if (headers[i].textContent === name) {
+            // console.log(name);
+            // console.log(headers[i].getBoundingClientRect());
+
+            // Compute top coordinate of header element
+            let y = headers[i].getBoundingClientRect().top;
+            console.log(y);
+
+            // Compute top header margin
+            let style =
+              headers[i].currentStyle || window.getComputedStyle(headers[i]);
+            let marginTop = parseInt(style.marginTop);
+
+            window.scrollTo(0, y - 64 - marginTop);
+            break;
+          }
+        }
+      }}
+    >
+      {name}
+    </span>
+  );
 };
 
 ReactDOM.render(<Application />, document.getElementById("root"));
